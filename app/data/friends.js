a6f99d3dd5849a916d22dfd0a413cde9a244605b
naz585 <naz585@aol.com>
2018-11-11 18:50:07 -0500

    files

---
+++ app/data/friends.js
@@ -0,0 +1,104 @@
+
+var friends = [
+    {
+      name: "Robert De Niro",
+      photo: "https://www.randomlists.com/img/people/robert_de_niro.jpg",
+      scores: [
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+      ]
+    },
+    {
+      name: "James Earl Jones",
+      photo: "https://www.randomlists.com/img/people/james_earl_jones.jpg",
+      scores: [
+        "4",
+        "2",
+        "5",
+        "1",
+        "3",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2"
+      ]
+    },
+    {
+      name: "Jeremiah Scanlon",
+      photo: "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+      scores: [
+        "5",
+        "2",
+        "2",
+        "2",
+        "4",
+        "1",
+        "3",
+        "2",
+        "5",
+        "5"
+      ]
+    },
+    {
+      name: "Tina Fey",
+      photo: "https://www.randomlists.com/img/people/tina_fey.jpg",
+      scores: [
+        "3",
+        "3",
+        "4",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2",
+        "2",
+        "3"
+      ]
+    },
+    {
+      name: "Leonardo DiCaprio",
+      photo: "https://www.randomlists.com/img/people/leonardo_dicaprio.jpg",
+      scores: [
+        "4",
+        "3",
+        "4",
+        "1",
+        "5",
+        "2",
+        "5",
+        "3",
+        "1",
+        "4"
+      ]
+    },
+    {
+      name: "Anne Hathaway",
+      photo: "https://www.randomlists.com/img/people/anne_hathaway.jpg",
+      scores: [
+        "4",
+        "4",
+        "2",
+        "3",
+        "2",
+        "2",
+        "3",
+        "2",
+        "4",
+        "5"
+      ]
+    }
+  
+  ];
+  
+  // Here we export the array. This makes it accessible to other files using require.
+  module.exports = friends;
+  
